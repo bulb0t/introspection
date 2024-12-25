56cf0ef207a33f84f7c3f5b9b1a59a926a0b61b1
bulb0t <yu.uglenko@yandex.ru>
2024-12-25 21:26:15 +0300

    Add files via upload

--- introspection.py
+++ introspection.py
@@ -4,6 +4,8 @@ from inspect import getmodule
 
 def introspection_info(obj):
     methods = [method for method in dir(obj) if callable(getattr(obj, method))]
-    return f'type: {type(obj).__name__}, attributes: {dir(obj)}, methods: {methods}, module: {getmodule(obj).__name__ if getmodule(obj) else '__main__'}'
+    attributes = [attr for attr in dir(obj) if not callable(getattr(obj, attr))]
+    return f'type: {type(obj).__name__}, attributes: {attributes}, methods: {methods}, module: {getmodule(obj).__name__ if getmodule(obj) else "__main__"}'
 
 pprint(introspection_info(5))
+
